017b216c3e38d8f2e05b830237198cd407b794bb
taenin <persons.matthew@gmail.com>
2016-12-07 18:05:23 -0800

    those colors tho

--- cutscenes.js
+++ cutscenes.js
@@ -167,6 +167,7 @@ var createCSEditor = function(){
 			childActions: [],
 			ActionType: worker.defaultActionType,
 			ActionSubtype: worker.defaultSubType,
+			style: parent.style ? "background-color: " + getTintedColor(parent.style, 10) : "background-color: " + getRandomColor(),
 		});
 		if(parent){
 			parent.childActions.push(actionId);
@@ -722,6 +723,31 @@ function download(filename, text) {
   document.body.removeChild(element);
 }
 
+function getTintedColor(color, v) {
+	color = color.replace("background-color: ", "");
+	color = color.replace(";", "");
+    if (color.length >6) { color= color.substring(1,color.length)}
+    var rgb = parseInt(color, 16); 
+    var r = Math.abs(((rgb >> 16) & 0xFF)+v); if (r>255) r=r-(r-255);
+    var g = Math.abs(((rgb >> 8) & 0xFF)+v); if (g>255) g=g-(g-255);
+    var b = Math.abs((rgb & 0xFF)+v); if (b>255) b=b-(b-255);
+    r = Number(r < 0 || isNaN(r)) ? 0 : ((r > 255) ? 255 : r).toString(16); 
+    if (r.length == 1) r = '0' + r;
+    g = Number(g < 0 || isNaN(g)) ? 0 : ((g > 255) ? 255 : g).toString(16); 
+    if (g.length == 1) g = '0' + g;
+    b = Number(b < 0 || isNaN(b)) ? 0 : ((b > 255) ? 255 : b).toString(16); 
+    if (b.length == 1) b = '0' + b;
+    return "#" + r + g + b + ";";
+} 
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color + ";";
+}
 
  //-----------------------------------------Initialize Page---------------------------------------
  var csEditor = {};
